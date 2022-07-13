2f52db0901eb2a828be816bdba39fed102999c23
paulminesnancy <paulc5830@gmail.com>
2022-07-13 17:15:57 +0200

    avancee

--- person.h
+++ person.h
@@ -2,16 +2,19 @@
 #define PERSON_H
 
 typedef struct _Person {
+    //a type person is defined by it's attributes src (where it comes from) and dest (where it goes) 
     int src;
     int dest;
 } Person;
 
 typedef struct _PersonList {
+    // a type representing a list of person using a linked list structure
     Person *person;
     struct _PersonList *next;
 } PersonList;
 
 Person* createPerson(int src, int dest);
 PersonList* insert(Person *p, PersonList *list);
+void affiche_perlist(PersonList *pl);
 
 #endif
